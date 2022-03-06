b0ff6cc1bdfcd157aa66ae21d688ebe9b6f199d3
barbar-a21904946 <61635416+barbar-a21904946@users.noreply.github.com>
2022-03-06 21:35:19 +0000

    add packages2+6.0

--- src/pt/ulusofona/cm/kotlin/challenge/exceptions/VeiculoDesligadoException.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/exceptions/VeiculoDesligadoException.kt
@@ -1,17 +1,3 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-class VeiculoDesligadoException{
-    var x:Int
-    var y:Int
-
-   constructor(x:Int , y :Int){
-       this.x=x;
-       this.y=y;
-   }
-fun alterarPositionException(){
-    throw Exception("Quanto tentas um movimento para a posição onde já te encontras.")
-}
-    override fun toString():String{
-        return "Posicao | x:0 | y:0"
-    }
-}
+class VeiculoDesligadoException(message: String) : Exception(message) {}
--- src/pt/ulusofona/cm/kotlin/challenge/exceptions/VeiculoLigadoException.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/exceptions/VeiculoLigadoException.kt
@@ -4,15 +4,6 @@ import java.util.*
 
 class VeiculoLigadoException {
     var identificador:String
-    lateinit var posicao: Posicao
-    lateinit var dataDeAquisicao:Date
 
-    constructor(identificador:String){
-        this.identificador=identificador
-    }
-    fun requerCarta():Boolean{
-        return true
-
-    }
 
 }
